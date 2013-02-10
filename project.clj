b38b62a71762fbfa81501d23ca251cbc11255610
James Phillips <jamesdphillips@gmail.com>
2013-02-10 14:40:05 -0800

    Name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dieter-handlebars "0.1.1"
+(defproject jamesdphillips/dieter-handlebars "0.1.1"
   :description "Compile handlebars templates"
   :dependencies [[dieter "0.3.0"]]
   :dev-dependencies [[org.clojure/clojure "1.4.0"]])
