f31e6fc1b7242b910a2de6b840b721e05fd0808d
James Phillips <jamesdphillips@gmail.com>
2013-02-10 15:11:18 -0800

    Update dieter verision

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jamesdphillips/dieter-handlebars "0.1.1"
   :description "Compile handlebars templates"
-  :dependencies [[dieter "0.3.0"]]
+  :dependencies [[jamesdphillips/dieter "0.3.1"]]
   :dev-dependencies [[org.clojure/clojure "1.4.0"]])
 
